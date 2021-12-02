cab7d5b0751686ecbbd9b534d12cec33ab5e2d75
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-12-02 11:37:32 -0300

    Add getId and setId

--- agenda-contatos/src/model/Contact.java
+++ agenda-contatos/src/model/Contact.java
@@ -50,6 +50,16 @@ public class Contact {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	@Override
 	public String toString() {
