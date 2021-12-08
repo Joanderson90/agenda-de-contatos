a21677350ea20b19acd1d084b9238f8dd08fe92c
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2021-12-08 10:10:19 -0300

    Update Main

--- agenda-contatos/src/phoneBook/Main.java
+++ agenda-contatos/src/phoneBook/Main.java
@@ -8,32 +8,29 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
-    
-    @Override
-    public void start(Stage stage) throws Exception {
-    	
-    	FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/view/MainScreen.fxml"));
-        Parent root = loader.load();
-        
-        Image applicationIcon = new Image(getClass().getResourceAsStream("/icons/iconeAplicacao.png"));
-        
-        stage.getIcons().add(applicationIcon);
-        
-        Scene scene = new Scene(root);
-        
-        scene.getStylesheets().add("/Styles/Styles.css");
-        
-        stage.setTitle("MainScreen");
-        //stage.setMaximized(true);
-        stage.setScene(scene);
-        stage.show();
-    }
 
-   
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/view/MainScreen.fxml"));
+		Parent root = loader.load();
+
+		Image applicationIcon = new Image(getClass().getResourceAsStream("/icons/iconeAplicacao.png"));
+
+		stage.getIcons().add(applicationIcon);
+
+		Scene scene = new Scene(root);
+
+		scene.getStylesheets().add("/Styles/Styles.css");
+
+		stage.setTitle("MainScreen");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 }
